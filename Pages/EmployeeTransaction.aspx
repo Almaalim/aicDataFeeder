4053a7290110d1ffb287a50e913fff808ff849c1
S50-62-35-11\almaalim <m.dassan@almaalim.com>
2018-04-12 08:12:56 -0700

    trans

---
+++ Pages/EmployeeTransaction.aspx
@@ -0,0 +1,105 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MainMasterPage.master" AutoEventWireup="true" CodeFile="EmployeeTransaction.aspx.cs" Inherits="Pages_EmployeeTransaction" culture="auto" meta:resourcekey="PageResource1" uiculture="auto" %>
+
+<%@ Register Assembly="AjaxControlToolkit" Namespace="AjaxControlToolkit" TagPrefix="cc1" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder2" runat="Server">
+    <asp:UpdatePanel ID="upnlMain" runat="server">
+        <ContentTemplate>
+            <div id="pageDiv" runat="server">
+                <asp:Panel ID="pnlSearch" runat="server" class="SearchPanel" meta:resourcekey="pnlSearchResource1">
+                    <div class="row">
+                        <div class="col1">
+                            <asp:Label ID="lblSearch" runat="server" Text="Search By :" meta:resourcekey="lblSearchResource1"></asp:Label>
+                        </div>
+
+                        <div class="col2">
+                            <asp:DropDownList ID="ddlSearch" runat="server" OnSelectedIndexChanged="ddlSearch_SelectedIndexChanged"
+                                AutoPostBack="True" meta:resourcekey="ddlSearchResource1">
+                                <asp:ListItem Value="[none]" Text="[none]" Selected="True" meta:resourcekey="ListItemResource1"></asp:ListItem>
+                                <asp:ListItem Value="EmpID" Text="Employee ID" meta:resourcekey="ListItemResource2"></asp:ListItem>
+                                <asp:ListItem Value="EmpNameAr" Text="Employee Name (Ar)" meta:resourcekey="ListItemResource3"></asp:ListItem>
+                                <asp:ListItem Value="EmpNameEn" Text="Employee Name (En)" meta:resourcekey="ListItemResource4"></asp:ListItem>
+                            </asp:DropDownList>
+                        </div>
+
+                        <div class="col1">
+                            <asp:TextBox ID="txtSearch" runat="server" Enabled="False" meta:resourcekey="txtSearchResource1"></asp:TextBox>
+                            <asp:RequiredFieldValidator ID="rvSearch" runat="server" ControlToValidate="txtSearch" CssClass="CustomValidator"
+                                EnableClientScript="False" ValidationGroup="vgSearch" Text="&lt;img src='../Images/icon/Exclamation.gif' title='You must enter a value to search!' /&gt;" meta:resourcekey="rvSearchResource1"></asp:RequiredFieldValidator>
+                            <cc1:FilteredTextBoxExtender ID="eFilteredSearch" runat="server" Enabled="True" FilterType="Numbers" TargetControlID="txtSearch" />
+                            <cc1:TextBoxWatermarkExtender ID="eWatermarkSearch" runat="server" Enabled="True" TargetControlID="txtSearch" WatermarkText="can't type" WatermarkCssClass="watermarked" />
+                        </div>
+                        <div class="col1">
+                            <asp:Label ID="lblMonth" runat="server" Text="Month:" Height="18px" meta:resourcekey="lblMonthResource1"></asp:Label>
+                        </div>
+                        <div class="col2">
+                            <asp:DropDownList ID="ddlMonth" runat="server" meta:resourcekey="ddlMonthResource1">
+                            </asp:DropDownList>
+                        </div>
+                        <div class="col1">
+                            <asp:Label ID="lblYear" runat="server" Text="Year:" Height="18px" meta:resourcekey="lblYearResource1"></asp:Label>
+                        </div>
+                        <div class="col2">
+                            <asp:DropDownList ID="ddlYear" runat="server" meta:resourcekey="ddlYearResource1">
+                            </asp:DropDownList>
+                        </div>
+                        <div class="col1">
+                            <asp:Button ID="btnSearch" runat="server" Text="Search" CssClass="btnSearch" OnClick="btnSearch_Click"
+                                ValidationGroup="vgSearch" Enabled="False" meta:resourcekey="btnSearchResource1" />
+                        </div>
+                    </div>
+                </asp:Panel>
+                <div class="row">
+                    <div class="col12">
+                        <asp:GridView ID="grdData" runat="server" AutoGenerateColumns="False" CssClass="GridTable"
+                            DataKeyNames="EmpID" Width="100%" GridLines="Horizontal" HorizontalAlign="Center"
+                            CellPadding="4" AllowPaging="True" OnDataBound="grdData_DataBound"
+                            OnRowDataBound="grdData_RowDataBound" OnSorting="grdData_Sorting" OnRowCreated="grdData_RowCreated"
+                            OnPageIndexChanging="grdData_PageIndexChanging" ShowFooter="True" meta:resourcekey="grdDataResource1">
+                            <Columns>
+                                <asp:BoundField DataField="EmpID" HeaderText="Employee ID " SortExpression="EmpID" meta:resourcekey="BoundFieldResource1">
+                                    <HeaderStyle HorizontalAlign="Center" VerticalAlign="Middle" />
+                                    <ItemStyle HorizontalAlign="Center" VerticalAlign="Middle" />
+                                </asp:BoundField>
+                                <asp:BoundField DataField="EmpNameAr" HeaderText="Employee Name (Ar)" SortExpression="EmpNameAr" meta:resourcekey="BoundFieldResource2">
+                                    <HeaderStyle HorizontalAlign="Center" VerticalAlign="Middle" />
+                                    <ItemStyle HorizontalAlign="Center" VerticalAlign="Middle" />
+                                </asp:BoundField>
+                                <asp:BoundField DataField="EmpNameEn" HeaderText="Employee Name (En)" SortExpression="EmpNameEn" meta:resourcekey="BoundFieldResource3">
+                                    <HeaderStyle HorizontalAlign="Center" VerticalAlign="Middle" />
+                                    <ItemStyle HorizontalAlign="Center" VerticalAlign="Middle" />
+                                </asp:BoundField>
+                                <asp:TemplateField HeaderText="Date" SortExpression="TrnDate" meta:resourcekey="TemplateFieldResource1">
+                                    <ItemTemplate>
+                                        <%# DisplayFun.GrdDisplayDate(Eval("TrnDate"), 'S')%>
+                                    </ItemTemplate>
+                                </asp:TemplateField>
+                                <asp:TemplateField HeaderText="Time" InsertVisible="False"
+                                    SortExpression="TrnTime" meta:resourcekey="TemplateFieldResource2">
+                                    <ItemTemplate>
+                                        <%# DisplayFun.GrdDisplayTime(Eval("TrnTime"))%>
+                                    </ItemTemplate>
+                                </asp:TemplateField>
+                                <asp:BoundField HeaderText="TrnType" DataField="TrnType"
+                                    ReadOnly="True" SortExpression="TrnType" meta:resourcekey="BoundFieldResource4"></asp:BoundField>
+                                <asp:TemplateField HeaderText="Type" InsertVisible="False"
+                                    SortExpression="TrnType1" meta:resourcekey="TemplateFieldResource3">
+                                    <ItemTemplate>
+                                        <%# DisplayFun.GrdDisplayTypeTrans(Eval("TrnType"))%>
+                                    </ItemTemplate>
+                                </asp:TemplateField>
+                                <asp:BoundField HeaderText="Location(En)" DataField="LocationEn"
+                                    InsertVisible="False" ReadOnly="True" meta:resourcekey="BoundFieldResource5"></asp:BoundField>
+                                <asp:BoundField HeaderText="Location(Ar)" DataField="LocationAr"
+                                    InsertVisible="False" ReadOnly="True" meta:resourcekey="BoundFieldResource6"></asp:BoundField>
+                            </Columns>
+                        </asp:GridView>
+                    </div>
+                </div>
+            </div>
+        </ContentTemplate>
+    </asp:UpdatePanel>
+</asp:Content>
+
---
+++ Pages/EmployeeTransaction.aspx.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.Collections;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+using System.Drawing;
+using System.Text;
+
+public partial class Pages_EmployeeTransaction : BasePage
+{
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    DataTable dt;
+    EmployeePro ProClass = new EmployeePro();
+    EmployeeSql SqlClass = new EmployeeSql();
+    DBFun DBCs = new DBFun();
+    DateFun DTCs = new DateFun();
+
+    string MainPer = "Emp";
+    string MainQuery = " SELECT * FROM EmpTransactionView WHERE EmpID = EmpID ";
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        //--Common Code----------------------------------------------------------------- //
+        FormSession.FillSession("Employees", pageDiv);
+        FormCtrl.RefreshGridEmpty(ref grdData, 20, "No Data Found", "لا توجد بيانات");
+        //--Common Code----------------------------------------------------------------- //
+
+        if (!IsPostBack)
+        {
+            pnlSearch.Enabled = grdData.Enabled = FormSession.PermUsr.Contains("VT" + MainPer);
+            FillGrid(MainQuery);
+
+            //DataItemStatus(false);
+            Fillddl();
+        }
+    }
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    public void Fillddl()
+    {
+        try
+        {
+            DTCs.YearPopulateList(ref ddlYear);
+            DTCs.MonthPopulateList(ref ddlMonth);
+        }
+        catch (Exception ex) { }
+    }
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    /*##############################################################################################################################*/
+    /*##############################################################################################################################*/
+    #region DataItem Events
+
+    void ClearData()
+    {
+        ViewState["CommandName"] = "";
+    }
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    void ClearItem()
+    {
+        grdData.SelectedIndex = -1;
+        FillInfoLabel("", "");
+        //DataItemStatus(false);
+        ButtonAction(true, "10000");
+        ClearData();
+    }
+
+    #endregion
+    /*##############################################################################################################################*/
+    /*##############################################################################################################################*/
+
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    /*##############################################################################################################################*/
+    /*##############################################################################################################################*/
+    #region SearchItem Events
+
+    public void SearchItemStatus(bool pBtn, bool pFiltered, string pTxtMark)
+    {
+        btnSearch.Enabled = pBtn;
+        txtSearch.Enabled = pBtn;
+        eWatermarkSearch.WatermarkText = pTxtMark;
+        eFilteredSearch.Enabled = pFiltered;
+    }
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    protected void ddlSearch_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        txtSearch.Text = "";
+
+        if (ddlSearch.SelectedValue == "[none]")
+        {
+            SearchItemStatus(false, false, "can't type");  /******/ Search();
+        }
+        else
+        {
+            if (ddlSearch.SelectedValue == "EmpID") { SearchItemStatus(true, false, "Type Employee ID here"); }
+            else if (ddlSearch.SelectedValue == "EmpNameAr") { SearchItemStatus(true, false, "Type Employee Name (Ar) here"); }
+            else if (ddlSearch.SelectedValue == "EmpNameEn") { SearchItemStatus(true, false, "Type Employee Name (En) here"); }
+
+            ClearItem();
+            FillGrid(MainQuery + " AND EmpID = '@@@@'");
+        }
+    }
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    protected void btnSearch_Click(object sender, EventArgs e)
+    {
+        if (ddlSearch.SelectedIndex > 0)
+        {
+            if (!Page.IsValid) { return; }
+            ClearItem();
+        }
+
+        Search();
+    }
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    protected void Search()
+    {
+        StringBuilder SQ = new StringBuilder();
+        SQ.Append(MainQuery);
+        if (ddlSearch.SelectedIndex > 0)
+        {
+            if (ddlSearch.SelectedValue == "EmpID") { SQ.Append(" AND EmpID = '" + txtSearch.Text + "'"); }
+            if (ddlSearch.SelectedValue == "EmpNameAr") { SQ.Append(" AND EmpNameAr LIKE '%" + txtSearch.Text + "%'"); }
+            if (ddlSearch.SelectedValue == "EmpNameEn") { SQ.Append(" AND EmpNameEn LIKE '%" + txtSearch.Text + "%'"); }
+        }
+
+        DateTime SDate = DateTime.Now;
+        DateTime EDate = DateTime.Now;
+        DTCs.FindMonthDates(ddlYear.SelectedValue, ddlMonth.SelectedValue, out SDate, out EDate);
+
+        SQ.Append(" AND (TrnDate between '" + SDate + "' AND '" + EDate + "')");
+
+        FillGrid(SQ.ToString());
+    }
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    void ClearSearch()
+    {
+        txtSearch.Text = "";
+        ddlSearch.SelectedIndex = -1;
+    }
+
+    #endregion
+    /*##############################################################################################################################*/
+    /*##############################################################################################################################*/
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    /*##############################################################################################################################*/
+    /*##############################################################################################################################*/
+    #region ButtonAction Events
+
+    protected void ButtonAction(bool pSearch, string pBtn) //string pBtn = [ADD,Edit,Delete,Save,Cancel]
+    {
+        if (FormSession.PermUsr.Contains("VT" + MainPer)) { pnlSearch.Enabled = pSearch; } else { pnlSearch.Enabled = false; }
+        if (FormSession.PermUsr.Contains("VT" + MainPer)) { grdData.Enabled = pSearch; } else { grdData.Enabled = false; }
+    }
+
+    #endregion
+    /*##############################################################################################################################*/
+    /*##############################################################################################################################*/
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    /*##############################################################################################################################*/
+    /*##############################################################################################################################*/
+    #region GridView Events
+
+    protected void grdData_DataBound(object sender, EventArgs e) { }
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    protected void grdData_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        try
+        {
+            GridViewRow gridrow = grdData.SelectedRow;
+            if (FormCtrl.isGridEmpty(gridrow.Cells[0].Text))
+            {
+                FormCtrl.FillGridEmpty(ref grdData, 20, "No Data Found", "لا توجد بيانات");
+            }
+            else
+            {
+                //PopulateData(gridrow.Cells[0].Text);
+            }
+        }
+        catch (Exception e1) { }
+    }
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    public void FillGrid(string Q)
+    {
+        dt = DBCs.FetchData(Q);
+        if (!DBCs.IsNullOrEmpty(dt) && FormSession.PermUsr.Contains("VT" + MainPer))
+        {
+            grdData.DataSource = (DataTable)dt;
+            grdData.DataBind();
+        }
+        else
+        {
+            FormCtrl.FillGridEmpty(ref grdData, 20, "No Data Found", "لا توجد بيانات");
+        }
+    }
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    protected void grdData_RowDataBound(object sender, GridViewRowEventArgs e) { FormCtrl.GridSelectedRow(this, this.grdData, e.Row); }
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    protected void FillInfoLabel(string pID, string pName)
+    {
+        if (string.IsNullOrEmpty(pID) == false) { pID = "              ID : " + pID + "-----------"; }
+        if (string.IsNullOrEmpty(pName) == false) { pName = "            Name : " + pName + "           "; }
+
+        //epnlData.CollapsedText = pID + pName + "(Show Details...)";
+        //epnlData.ExpandedText = pID + pName + "(Hide Details)";
+    }
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    protected void grdData_PageIndexChanging(object sender, GridViewPageEventArgs e)
+    {
+        grdData.PageIndex = e.NewPageIndex;
+        ClearItem();
+        Search();
+    }
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    protected void grdData_Sorting(object sender, GridViewSortEventArgs e) { ClearItem(); }
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    protected void grdData_RowCreated(object sender, GridViewRowEventArgs e)
+    {
+        try
+        {
+            //e.Row.Cells[11].Visible = false;
+        }
+        catch { }
+    }
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    public string getStatus(object pValue)
+    {
+        if (pValue != null)
+        {
+            if (pValue.ToString() == "True") { return General.Msg("Active", "فعال"); }
+            if (pValue.ToString() == "False") { return General.Msg("InActive", "غير فعال"); }
+        }
+
+        return null;
+    }
+
+    #endregion
+    /*##############################################################################################################################*/
+    /*##############################################################################################################################*/
+
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+}
